cabe5390d48739275b73469af04533b96ead3fb5
Diego Reis <diego.luis@lavid.ufpb.br>
2022-03-10 15:53:43 -0300

    bst logger

--- Binary Search Tree/BSTStructure.h
+++ Binary Search Tree/BSTStructure.h
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "Utils/syntaxUtil.h"
 
 typedef struct bstNode BSTNode;
 
@@ -11,3 +12,13 @@ struct bstNode
     BSTNode* left;
     BSTNode* right;
 };
+
+void printBSTNode(bstNode* node)
+{
+    if(node == null)
+        return;
+
+    printBSTNode(node->left);
+    printf("%d ", node->info);
+    printBSTNode(node->right);
+}
